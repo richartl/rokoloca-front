2c8a8cfa2e7075630cffe6bd83985cf074b3ee81
ripper <ricardo.pcan@gmail.com>
2018-06-27 21:10:01 -0500

    WIP init new container

---
+++ src/containers/MainPlayerContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class MainPlayerContainer extends React.Component {
+
+  render() {
+    return (
+
+    )
+  }
+
+}
+
+export default MainPlayerContainer;
