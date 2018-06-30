f80288db69a9a7e7caff18944ae3a11cf321a905
ripper <ricardo.pcan@gmail.com>
2018-06-30 01:01:00 -0500

    Add actioncable

--- src/containers/MainPlayerContainer/index.js
+++ src/containers/MainPlayerContainer/index.js
@@ -2,10 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { NavBar } from 'components'
 import ReactPlayer from 'react-player'
+import actionCable from 'actioncable'
 import './index.css'
 
 
 class MainPlayerContainer extends React.Component {
+
+  createSocket() {
+    let cable = actionCable.createConsumer('ws://localhost:5000/cable');
+    this.playlists = cable.subscriptions.create({
+      channel: 'PlaylistChannel',
+      uuid: 'a05b9d29-8d84-4f99-b7ad-d180c8b7be96'
+    },
+    {
+      connected: () => {
+        console.log('Conected!');
+      },
+      received: (data) => {
+        console.log(data);
+      },
+      create: function(chatContent) {
+        this.perform('create', {
+          content: chatContent
+        });
+      }
+    });
+  }
+
+  componentWillMount() {
+    this.createSocket();
+  }
 
   render() {
     return (
